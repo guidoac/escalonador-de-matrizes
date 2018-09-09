8ec3687e02285743949a69095d95ad5ea3a5f601
guidoac <guido.ptr13@gmail.com>
2018-09-08 23:56:36 -0300

    a

--- escalonar_matriz.py
+++ escalonar_matriz.py
@@ -32,15 +32,14 @@ class Escalonador():
             for elem in linha:
                 if elem !=0 and j < i:
                     coluna = self.getColuna(j)
-                    for coef in np.flip(coluna[:i-1]):
-                        if coef != 0:
-                            print('formula: {0} * {1} - {2} * {3}'.format(coef, self.matriz_final[i], elem, self.matriz_final[linha_esc]))
-                            pivo_ant = self.getPivo(i-1)
-                            linha_res = coef * self.matriz_final[i] - elem * self.matriz_final[linha_esc]
-                            self.matriz_final = np.delete(self.matriz_final, i,0)
-                            self.matriz_final = np.insert(self.matriz_final, i, linha_res, axis=0)
-                            print (self.matriz_final)
-                            self.escalonar()
+                    coefi, linha_esc = self.escolher_coef(coluna, i)
+                    print('formula: {0} * {1} - {2} * {3}'.format(coefi, self.matriz_final[i], elem, self.matriz_final[linha_esc]))
+                    pivo_ant = self.getPivo(i-1)
+                    linha_res = coefi * self.matriz_final[i] - elem * self.matriz_final[linha_esc]
+                    self.matriz_final = np.delete(self.matriz_final, i,0)
+                    self.matriz_final = np.insert(self.matriz_final, i, linha_res, axis=0)
+                    print (self.matriz_final)
+                    self.escalonar()
                 elif elem == 0 and j==i-1:
                     break
                 else:
@@ -50,17 +49,12 @@ class Escalonador():
             #print ('Linha {0}: {1}, pivo ant: {2}'.format(i, linha, coluna, coef))
             i+=1
 
-    def escolher_coef(self, col):
-        index = 0
-        for coef in np.flip(col)[1:]:
-            print (np.flip(col)[1:])
-            print (coef)
-            if coef!= 0:
-                break
-            else:
-                pass
-            index +=1
-        return coef, index
+    def escolher_coef(self, col, linha_atual):
+        if len(col) == 1:
+            return col
+        else:
+
+            return coef, linha_esc
 
 matriz_e = Escalonador(arq)
 print(matriz_e.matriz_final)
