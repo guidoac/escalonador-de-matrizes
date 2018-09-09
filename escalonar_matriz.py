276254bf5633b778ef9447eaa048ba0aba4f1a21
guidoac <guido.ptr13@gmail.com>
2018-09-09 12:33:52 -0300

    Revert "a"
    This reverts commit d9d13a4b44b0c663abef90dfc55fa481b6faf7b6.

--- escalonar_matriz.py
+++ escalonar_matriz.py
@@ -26,6 +26,9 @@ class Escalonador():
                 linha_int.append(int(elem))
             matriz_f.append(linha_int)
         self.matriz_final = np.array(matriz_f)
+        self.cont_escal = 0
+        print('shape: {}'.format(self.matriz_final.shape))
+
 
     def getColuna(self, col):
         return np.array(self.matriz_final[:,col])
@@ -40,13 +43,14 @@ class Escalonador():
         #inicio as variaveis contadoras. i = linha (começa com 1 pois quero escalonar apenas as linhas 1 em diante). j= elemento que precisa ser zerado
         i = 1
         #percorre cada linha da matriz_final a partir da linha 1, pois não escalonamos a linha 1
-<<<<<<< HEAD
-        while i < len(self.matriz_final):
-            for linha in self.matriz_final[1:]:
-                #percorre cada elemento da linha a ser escalonada a procura de um elemento diferente de 0 e que vem antes do pivo daquela linha
-                j=0
+        for linha in self.matriz_final[1:]:
+            #percorre cada elemento da linha a ser escalonada a procura de um elemento diferente de 0 e que vem antes do pivo daquela linha
+            if i > len(self.matriz_final):
+                print('----------- escalonamento terminado! ------------')
+            else:
+                j = 0
                 for elem in linha:
-                    if elem !=0:
+                    if elem !=0 and j < i:
                         coluna = self.getColuna(j)
                         pivo = self.getPivo(j)
                         print(coluna)
@@ -56,38 +60,16 @@ class Escalonador():
                         self.matriz_final = np.delete(self.matriz_final, i,0)
                         self.matriz_final = np.insert(self.matriz_final, i, linha_res, axis=0)
                         #troco a linha_res pela linha a ser substituida na matriz_final. E executo a função da forma recursiva para finalizar a matriz.
+                        print ('i = {0}, j = {1}'.format(i, j))
                         print (self.matriz_final)
                         self.escalonar()
-                    else:
-                        j+=1
+                    elif j == i:
+                        break
+                    elif elem == 0:
                         pass
+                    j+=1
                 i+=1
-        print('------------------ escalonamento terminado ---------------------')
-=======
-        for linha in self.matriz_final[1:]:
-            #percorre cada elemento da linha a ser escalonada a procura de um elemento diferente de 0 e que vem antes do pivo daquela linha
-            j = 0
-            for elem in linha:
-                if elem !=0 and j < i:
-                    coluna = self.getColuna(j)
-                    pivo = self.getPivo(j)
-                    print(coluna)
-                    print('formula: {0} * {1} - {2} * {3}'.format(pivo, self.matriz_final[i], elem, self.matriz_final[j]))
-                    #linha_res é a linha que vai substituir na matriz. com as devidas operações feitas para zerar seus elementos que precisam ser zerados
-                    linha_res = pivo * self.matriz_final[i] - elem * self.matriz_final[j]
-                    self.matriz_final = np.delete(self.matriz_final, i,0)
-                    self.matriz_final = np.insert(self.matriz_final, i, linha_res, axis=0)
-                    #troco a linha_res pela linha a ser substituida na matriz_final. E executo a função da forma recursiva para finalizar a matriz.
-                    print (self.matriz_final)
-                    self.escalonar()
-                elif elem == 0 and j==i-1:
-                    break
-                else:
-                    pass
-                j+=1
 
-            i+=1
->>>>>>> parent of d9d13a4... a
 
 matriz_e = Escalonador(arq)
 print(matriz_e.matriz_final)
